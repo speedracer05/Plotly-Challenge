91dedd03e8496d4f92a2d34f52e7ff1362b13e4b
speedracer05 <speedracer-05@outlook.com>
2021-09-10 02:00:53 -0700

    add bubble chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -23,30 +23,33 @@ d3.json("samples.json").then((response) => {
 /* Collect demographic data; loop over object; create a new paragraph for
 each key/value--for demographics panel */
 function buildMetadata(sample) {
-
-    d3.json("samples.json").then((response) => {
-        var metadata = response.metadata;
-        var siftData = metadata.filter(meta => meta.id == sample)[0];
-        var panel = d3.select("#sample-metadata");
-        panel.html("");
-        Object.entries(siftData).forEach(([key, value]) => {
-            panel.append("p")
-                .text(`${key}: ${value}`);
-        });
-    });
+  d3.json("samples.json").then((response) => {
+      var metadata = response.metadata;
+      var siftData = metadata.filter(meta => meta.id == sample)[0];
+      var panel = d3.select("#sample-metadata");
+      panel.html("");
+      Object.entries(siftData).forEach(([key, value]) => {
+          panel.append("p")
+              .text(`${key}: ${value}`);
+      });
+  });
 }
+
+// Collect samples data
 function buildCharts(sample) {
   d3.json("samples.json").then((response) => {
-      var samples = response.samples;
-      var sampleData = samples.filter(button => button.id == sample)[0];
-
-     // Create bar chart 
-      var data = [{
-          type: 'bar',
-          x: sampleData.sample_values.slice(0, 10).reverse(),
-          y: sampleData.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse(),
-          text: sampleData.otu_labels.slice(0, 10).reverse(),
-          orientation: 'h'
-      }];
-    });
+    var samples = response.samples;
+    var sampleData = samples.filter(button => button.id == sample)[0];
+    // Create bar chart with hover text; retrieve first 10 items; set chart horizontal
+    var data = [{
+      type: 'bar',
+      x: sampleData.sample_values.slice(0, 10).reverse(),
+      y: sampleData.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse(),
+      text: sampleData.otu_labels.slice(0, 10).reverse(),
+      orientation: 'h',
+      marker: {
+        color: 'RdBu'
+      }
+    }];
+  });
 }
