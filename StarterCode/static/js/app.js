a043a0f824c2d54f97f61f119c3f4a01ca337395
speedracer05 <speedracer-05@outlook.com>
2021-09-09 21:20:55 -0700

    fixed missing curly braces and parthensis

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,6 @@
 // select userinput field
 var select = d3.select("#selDataset")
+console.log(select)
 
 // Create function to get data from JSON file
 function int() {
@@ -17,6 +18,35 @@ function int() {
     buildMetadata(names[0]);
     buildCharts(names[0]);
     buildGauge(names[0]);
-    console.log(select)
 });
+}
+
+function buildMetadata(sample) {
+
+    d3.json("samples.json").then((response) => {
+        var metadata = response.metadata;
+        var filteredData = metadata.filter(meta => meta.id == sample)[0];
+
+        var panel = d3.select("#sample-metadata");
+        panel.html("");
+        Object.entries(filteredData).forEach(([key, value]) => {
+            panel.append("p")
+                .text(`${key}: ${value}`);
+        });
+    });
+}
+function buildCharts(sample) {
+  d3.json("samples.json").then((response) => {
+      var samples = response.samples;
+      var sampleData = samples.filter(button => button.id == sample)[0];
+
+     // Create bar chart 
+      var data = [{
+          type: 'bar',
+          x: sampleData.sample_values.slice(0, 10).reverse(),
+          y: sampleData.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse(),
+          text: sampleData.otu_labels.slice(0, 10).reverse(),
+          orientation: 'h'
+      }];
+    });
 }
