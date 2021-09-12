fbabc173c81fc317e3313905a03ee6740ac94fa8
speedracer05 <speedracer-05@outlook.com>
2021-09-12 08:53:48 -0700

    add dashboard image, html comment to gauge, uncomment custom gauge code, start modifying for value ranging 0 - 9

--- static/js/app.js
+++ static/js/app.js
@@ -74,7 +74,7 @@ function buildCharts(sample) {
         color: sampleData.otu_ids,
         size: sampleData.sample_values,
         colorscale: 'Portland'
-      }
+      },
     };
 
     var data2 = [trace1];
@@ -82,6 +82,8 @@ function buildCharts(sample) {
       font: { color: "black", family: "Helvetica" },
       title: 'Bacteria Found in Belly Button: Bubble Chart',
       showlegend: false,
+      xaxis: { title: "OTU ID"},
+      yaxis: {title: "Sample Value"}
   };
 
 
@@ -98,40 +100,41 @@ function optionChanged(newSample) {
 
 init();
 
-// Custom Gauge Chart
-/* need to modify */
-// var data = [
-//   {
-//     type: "indicator",
-//     mode: "gauge+number+delta",
-//     value: 420,
-//     title: { text: "Speed", font: { size: 24 } },
-//     delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
-//     gauge: {
-//       axis: { range: [null, 500], tickwidth: 1, tickcolor: "darkblue" },
-//       bar: { color: "darkblue" },
-//       bgcolor: "white",
-//       borderwidth: 2,
-//       bordercolor: "gray",
-//       steps: [
-//         { range: [0, 250], color: "cyan" },
-//         { range: [250, 400], color: "royalblue" }
-//       ],
-//       threshold: {
-//         line: { color: "red", width: 4 },
-//         thickness: 0.75,
-//         value: 490
-//       }
-//     }
-//   }
-// ];
+    // Advanced Challenge Assignment (Optional) 
+    // Adapt the Gauge Chart from https://plot.ly/javascript/gauge-charts/ 
+    // to plot the weekly washing frequency of the individual.
+    var data = [
+      {
+        type: "indicator",
+        mode: "gauge+number+delta",
+        value: 420,
+        title: { text: "Speed", font: { size: 24 } },
+        delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
+        gauge: {
+          axis: { range: [null, 500], tickwidth: 1, tickcolor: "darkblue" },
+          bar: { color: "darkblue" },
+          bgcolor: "white",
+          borderwidth: 2,
+          bordercolor: "gray",
+          steps: [
+            { range: [0, 250], color: "cyan" },
+            { range: [250, 400], color: "royalblue" }
+          ],
+          threshold: {
+            line: { color: "red", width: 4 },
+            thickness: 0.75,
+            value: 490
+          }
+        }
+      }
+    ];
 
-// var layout = {
-//   width: 500,
-//   height: 400,
-//   margin: { t: 25, r: 25, l: 25, b: 25 },
-//   paper_bgcolor: "lavender",
-//   font: { color: "darkblue", family: "Arial" }
-// };
+    var layout = {
+      width: 500,
+      height: 400,
+      margin: { t: 25, r: 25, l: 25, b: 25 },
+      paper_bgcolor: "lavender",
+      font: { color: "darkblue", family: "Arial" }
+    };
 
-// Plotly.newPlot('myDiv', data, layout);
+    Plotly.newPlot('gauge', data, layout);
